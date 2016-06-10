71aabde21f73ef01513d3934b1d4f4f11882fb67
Adam Duston <aduston@gmail.com>
2016-06-10 02:43:17 -0400

    Made bars appear

--- HeartAppRN/ios/HeartApp/ChartView.m
+++ HeartAppRN/ios/HeartApp/ChartView.m
@@ -12,11 +12,32 @@
   [[UIColor whiteColor] setFill];
   UIRectFill(rect);
   
-  [[UIColor blueColor] setFill];
-  UIRectFill(CGRectMake(10, 10, 100, 300));
+  int i;
   
-  [[UIColor redColor] setFill];
-  UIRectFill(CGRectMake(120, 50, 100, 250));
+  CGFloat padding = 3.0;
+  CGFloat barLeft = 0.0;
+  CGFloat barWidth = ((rect.size.width - barLeft) / _data.count) - padding;
+  
+  int minValue = 999;
+  int maxValue = 0;
+  for (i = 0; i < _data.count; i++) {
+    int intValue = [[_data objectAtIndex:i] intValue];
+    if (intValue < minValue) {
+      minValue = intValue;
+    }
+    if (intValue > maxValue) {
+      maxValue = intValue;
+    }
+  }
+  
+  [[UIColor blueColor] setFill];
+  for (i = 0; i < [_data count]; i++) {
+    CGFloat barHeight = rect.size.height * ((CGFloat)[[_data objectAtIndex:i] intValue] / (CGFloat)(maxValue - minValue));
+    UIRectFill(CGRectMake(barLeft + (barWidth + padding) * i,
+                          rect.size.height - barHeight,
+                          barWidth,
+                          barHeight));
+  }
 }
 
 @end
