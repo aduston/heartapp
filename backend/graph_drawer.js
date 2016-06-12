6eb5c2c912187dc077d7db320575c5d2a8336bc1
Adam Duston <aduston@gmail.com>
2016-06-12 19:48:44 -0400

    Seems like a good solution for drawing graphs

--- backend/graph_drawer.js
+++ backend/graph_drawer.js
@@ -14,7 +14,6 @@ class ImageDrawer {
     this._bottomMargin = 40;
     this._leftMargin = 30;
     this._rightMargin = 30;
-    this._barPadding = 3;
     this._minBarHeight = 10;
 
     this._minValue = Math.min.apply(Math, this._observations);
@@ -25,31 +24,66 @@ class ImageDrawer {
     if (this._minValue % 10 != 0) {
       this._minValue -= (this._minValue % 10);
     }
-    this._barWidth = Math.floor(
-      (this._width - this._leftMargin - this._rightMargin - this._barPadding) /
-        this._observations.length) - this._barPadding;
+    this._barWidth =
+      (this._width - this._leftMargin - this._rightMargin) / this._observations.length;
     var maxBarHeight = this._height - this._topMargin - this._bottomMargin - this._minBarHeight; 
     this._beatHeight = maxBarHeight / (this._maxValue - this._minValue);
   }
 
   draw(output, done) {
-    this._drawBars();
+    this._drawValues();
     this._drawHorizontalLines();
     this._drawTimes();
     this._img.write(output, done);
   }
 
-  _drawBars() {
+  _drawValues() {
+    if (this._barWidth > 1.0) {
+      this._drawWideValues();
+    } else {
+      var img = this._img.stroke('#00f', 0.5);
+      var totalWidth = this._width - this._leftMargin - this._rightMargin
+      for (var i = 0; i < totalWidth; i++) {
+        var minMax = this._minAndMaxAt(i, totalWidth);
+        var min = minMax[0];
+        var max = minMax[1];
+        var x = this._leftMargin + i;
+        var y0 = this._height - (this._bottomMargin + this._minBarHeight +
+                                 (max - this._minValue) * this._beatHeight);
+        img = img.drawLine(x, y0, x, y0 + (max - min) * this._beatHeight);
+      }
+    }
+  }
+
+  _minAndMaxAt(partition, numPartitions) {
+    var minIndex = Math.floor((partition / numPartitions) * this._observations.length);
+    var maxIndex = Math.ceil(((partition + 1) / numPartitions) * this._observations.length);
+    var min = 999;
+    var max = 0;
+    for (var i = minIndex; i < maxIndex; i++) {
+      if (this._observations[i] < min) {
+        min = this._observations[i];
+      }
+      if (this._observations[i] > max) {
+        max = this._observations[i];
+      }
+    }
+    return [min, max];
+  }
+
+  _drawWideValues() {
+    var img = this._img.stroke('#00f', 0.5);
+    var lastX0, lastY0;
     for (var i = 0; i < this._observations.length; i++) {
-      var x0 = this._leftMargin + this._barPadding + (this._barWidth + this._barPadding) * i;
+      var x0 = this._leftMargin + this._barWidth * i;
       var y0 = this._height - (this._bottomMargin + this._minBarHeight +
                                (this._observations[i] - this._minValue) * this._beatHeight);
-      this._img.fill("#00f").drawRectangle(
-        x0,
-        y0,
-        x0 + this._barWidth,
-        this._height - (this._bottomMargin - this._minBarHeight)
-      );
+      img = img.drawLine(x0, y0, x0 + this._barWidth, y0);
+      if (i > 0) {
+        img = img.drawLine(lastX0, lastY0, x0, y0);
+      }
+      lastX0 = x0 + this._barWidth;
+      lastY0 = y0;
     }
   }
 
@@ -58,10 +92,10 @@ class ImageDrawer {
       if (this._maxValue - this._minValue > 40 && hr % 5 != 0) {
         continue;
       }
-      var labeledLine = (this._maxValue - this._minValue < 60 && i % 5 == 0) || hr % 10 == 0;
+      var labeledLine = (this._maxValue - this._minValue < 60 && hr % 5 == 0) || hr % 10 == 0;
       var y = this._height - (this._bottomMargin + this._minBarHeight + (hr - this._minValue) *
                               this._beatHeight);
-      this._img.stroke((labeledLine ? "#333" : "#333"), 1).drawLine(
+      this._img.stroke((labeledLine ? "#000" : "#333"), 0.5).drawLine(
         this._leftMargin,
         y,
         this._width - this._rightMargin,
--- backend/graph_drawer_main.js
+++ backend/graph_drawer_main.js
@@ -2,10 +2,13 @@
 
 var drawer = require('./graph_drawer');
 
-var hr = new Uint8Array(60 * 8);
+var hr = new Uint8Array(60 * 45);
 
-for (var i = 0; i < 60 * 8; i++) {
-  hr[i] = 70 + Math.random() * 60;
+hr[0] = 100;
+
+for (var i = 1; i < 60 * 45; i++) {
+  hr[i] = Math.floor(hr[i - 1] + (Math.random() - 0.40) * 5);
+  hr[i] = Math.min(180, Math.max(60, hr[i]));
 }
 
 drawer.draw(1200, 400, hr, null, "/Users/aduston/hr.png", function(err) { console.log("done", err); });
